b3ba3fa37ea252023a0a0f572b7bda6fe075eeea
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-01-06 15:34:55 +0100

    test: tools

---
+++ tools/tools_test.go
@@ -0,0 +1,37 @@
+package tools_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/fredrikaverpil/bld/tools/golangcilint"
+	"github.com/fredrikaverpil/bld/tools/govulncheck"
+	"github.com/fredrikaverpil/bld/tools/mdformat"
+	"github.com/fredrikaverpil/bld/tools/uv"
+)
+
+var tools = []struct {
+	name        string
+	prepare     func(context.Context) error
+	run         func(context.Context, ...string) error
+	versionArgs []string
+}{
+	{"golangci-lint", golangcilint.Prepare, golangcilint.Run, []string{"version"}},
+	{"govulncheck", govulncheck.Prepare, govulncheck.Run, []string{"-version"}},
+	{"uv", uv.Prepare, uv.Run, []string{"--version"}},
+	{"mdformat", mdformat.Prepare, mdformat.Run, []string{"--version"}},
+}
+
+func TestTools(t *testing.T) {
+	for _, tool := range tools {
+		t.Run(tool.name, func(t *testing.T) {
+			ctx := context.Background()
+			if err := tool.prepare(ctx); err != nil {
+				t.Fatalf("Prepare: %v", err)
+			}
+			if err := tool.run(ctx, tool.versionArgs...); err != nil {
+				t.Fatalf("Run %v: %v", tool.versionArgs, err)
+			}
+		})
+	}
+}
